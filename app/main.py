d019d22e395eb965495ae6e4667bdafe03136bc3
Bhavik Charola <bhavikcharola@gmail.com>
2024-10-05 01:45:43 +0530

    fix: change CORS url with ip port

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 origins = [
     "http://localhost",  # Allow localhost for development
     "http://localhost:5173",  # Allow frontend apps running on a different port (e.g., Vite or other dev tools)
-    "http://0.0.0.0:8001",
+    "http://0.0.0.0:8000",
     # Add other origins as needed, such as your production frontend URL
 ]
 
