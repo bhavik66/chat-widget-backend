9d9b68dbb1b1afe8543a3389f6bc86ee23b4c599
Bhavik Charola <bhavikcharola@gmail.com>
2024-10-05 13:13:13 +0530

    feat(ai): integrate gemini model in chatbot

--- app/utils/chatbot_logic.py
+++ app/utils/chatbot_logic.py
@@ -1,28 +1,39 @@
-
+import os
 from typing import Dict
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+load_dotenv()
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+# Create an instance of the LLM, using the 'gemini-pro' model with a specified creativity level
+llm = ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.9, api_key=google_api_key)
+
+# Set up a prompt template
+prompt = PromptTemplate.from_template('You are a content creator. Write me a tweet about {topic}')
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
 
 def generate_ai_response(user_message: str) -> Dict:
-    # Example logic to include actions based on user message
-    if "start" in user_message.lower():
-        response = {
-            "content": "Let's get started! What would you like to do next?",
-            "actions": {
-                "action1": "Learn More",
-                "action2": "Get Help"
-            }
+    response = chain.invoke(input=user_message)
+    response_text = response.get('text')
+    if "Learn More" in response_text:
+        actions = {
+            "action1": "Learn More",
+            "action2": "Get Help"
         }
-    elif "help" in user_message.lower():
-        response = {
-            "content": "How can I assist you today?",
-            "actions": {
-                "action1": "Account Issues",
-                "action2": "Technical Support"
-            }
+    elif "Account Issues" in response_text:
+        actions = {
+            "action1": "Account Issues",
+            "action2": "Technical Support"
         }
     else:
-        response = {
-            "content": f"AI response to: {user_message}",
-            "actions": None
-        }
+        actions = None
+
+    response = {
+        "content": response_text,
+        "actions": actions
+    }
     return response
